41fbbb6596a62ba5946ef8c846f16d4b93e276be
unknown <ondrej.richnak@gmail.com>
2022-10-15 14:18:50 +0200

    nasledujte subor settup local host.txt

---
+++ client/src/components/demo_component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const Demo = () => {
+    return ( 
+    <div className='text-red-700 font-bold text-center'>
+        toto je demo component
+    </div> 
+    );
+}
+
+export default Demo;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+// Vytvorenie Express aplikacie
+const express = require("express");
+const app = express();
+app.use(express.json()) // req.body
+
+// db 
+const DB = require("./db")
+const db = DB.getDbServiceInstance();
+
+// jednoducha api
+app.get("/api", (req,res) => {
+    res.json({"skuska":"ano fungujem"})
+} )
+
+app.get("/apidb", async (req,res) => {
+    try {
+        const resoult = await db.get_test();
+        console.log(resoult)
+        res.json(resoult);
+    } catch (err) {
+        console.log(err.massage);
+    }
+} )
+
+app.post("/apidb2", async (req,res) => {
+    try {
+        const {maximum} = req.body;
+        const resoult = await db.get_test2(maximum);
+        console.log(resoult)
+        res.json(resoult);
+    } catch (err) {
+        console.log(err.massage);
+    }
+} )
+const PORT = 5000; 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
