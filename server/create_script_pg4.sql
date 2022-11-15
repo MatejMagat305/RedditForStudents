cb49a8f932c8e20fa618dd29c4c84498678d453c
unknown <ondrej.richnak@gmail.com>
2022-11-15 15:30:10 +0100

    dopnenie suborov za M.Kris

--- server/create_script_pg4.sql
+++ server/create_script_pg4.sql
@@ -6,3 +6,35 @@ CREATE TABLE students (
                           nick_name varchar unique
 );
 select * from students;
+
+DROP TABLE IF EXISTS posts CASCADE;
+CREATE TABLE posts (
+                          id serial primary key,
+						  title varchar,
+                          post_text varchar,
+                          student_id integer references students,
+						  changed timestamp,
+                          flag boolean
+);
+select * from posts;
+
+DROP TABLE IF EXISTS images CASCADE;
+CREATE TABLE images (
+                          id serial primary key,
+                          alt varchar,
+                          post_id integer references posts
+						  
+);
+select * from images;
+
+DROP TABLE IF EXISTS raitins CASCADE;
+CREATE TABLE raitings (
+                          id serial primary key,
+                          category int check(category >=0 and category <=2),
+                          post_id integer references posts,
+						  student_id integer references students,
+					      unique (post_id, student_id)
+	
+						  
+);
+select * from raitings;
